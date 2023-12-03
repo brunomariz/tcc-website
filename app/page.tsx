6189c5bb86e713a319b3661e36bbcfebfe99f269
Bruno Mariz <bmarizot@gmail.com>
2023-12-03 16:20:33 -0300

    scaffold

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import Image from "next/image";
 
-type Props = {}
-
-export default function About() {
-    return <div>Abouta</div>
+export default function Home() {
+  return (
+    <main className="capa bg-white">
+      <div className="p-24">
+        <h1>
+          Shaders de pós-processamento visual open-source para Unreal Engine 5
+        </h1>
+      </div>
+      <div className="p-24">
+        <p>por</p>
+        <p>Bernardo Coutinho</p>
+        <p>Bruno Mariz</p>
+        <p>Vinicius Ariel</p>
+      </div>
+      <div className="p-24">
+        <p>São Paulo, SP</p>
+        <p>2023</p>
+      </div>
+    </main>
+  );
 }
